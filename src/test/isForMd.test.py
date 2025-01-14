98b000f311b9d24df2d5692de386ca1a9bd53fcf
hari64 <hari64boli64@gmail.com>
2025-01-14 17:55:57 +0900

    update isForMd.test.py

--- src/test/isForMd.test.py
+++ src/test/isForMd.test.py
@@ -33,11 +33,27 @@ def getMarkDown():
     return isForMd
 
 
+def getLintMd():
+    rulesInSample = set()
+    with open(curPath + "/../../sample/lint.md", encoding="utf-8") as f:
+        lines = f.readlines()
+        basename = None
+        for line in lines:
+            if line.startswith("## LL"):
+                basename = line.split(" ")[1].strip()
+                rulesInSample.add(basename)
+    return rulesInSample
+
+
 def main():
     byLLFiles = getLLFiles()
     byMarkDown = getMarkDown()
+    rulesInSample = getLintMd()
 
     assert byLLFiles == byMarkDown, set(byLLFiles.items()) ^ set(byMarkDown.items())
+
+    rulesByMd = set(k for k, v in byMarkDown.items() if v)
+    assert rulesByMd == rulesInSample, rulesByMd ^ rulesInSample
 
     print("isForMd.test.py ok!")
 

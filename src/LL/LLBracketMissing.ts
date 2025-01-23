69283f1bc5eb2044408237ad9ec4d016b0a1a83b
hari64 <hari64boli64@gmail.com>
2025-01-24 08:58:14 +0900

    fix the markdown problem of LLBracketMissing and LLT

--- src/LL/LLBracketMissing.ts
+++ src/LL/LLBracketMissing.ts
@@ -2,9 +2,10 @@ import * as vscode from 'vscode';
 import regex2diagnostics from '../util/regex2diagnostics';
 
 export default function LLBracketMissing(doc: vscode.TextDocument, txt: string): vscode.Diagnostic[] {
+    // In Markdown, `[^abc]` is a link to the anchor `abc`.
     return regex2diagnostics(
         doc, txt,
         "LLBracketMissing",
-        /[\^_][0-9A-Za-z]{2}/g
+        /(?<!\[)[\^_][0-9A-Za-z]{2}/g
     );
 }
--- src/LL/LLT.ts
+++ src/LL/LLT.ts
@@ -2,9 +2,10 @@ import * as vscode from 'vscode';
 import regex2diagnostics from '../util/regex2diagnostics';
 
 export default function LLT(doc: vscode.TextDocument, txt: string): vscode.Diagnostic[] {
+    // In Markdown, `[^abc]` is a link to the anchor `abc`.
     return regex2diagnostics(
         doc, txt,
         "LLT",
-        /\^T/g,
+        /(?<!\[)\^T/g,
     );
 }

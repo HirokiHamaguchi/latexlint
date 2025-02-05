b525ba60f6bec8854e03bd8a3298dbf49609f8c8
hari64 <hari64boli64@gmail.com>
2025-02-05 10:58:11 +0900

    update test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -68,7 +68,7 @@ async function testEnumerateDiagnostics(fileName: string, expected: number) {
 }
 
 async function testEnumerateDiagnosticsTex() {
-	await testEnumerateDiagnostics("sample/lint.tex", 103);
+	await testEnumerateDiagnostics("sample/lint.tex", 105);
 }
 
 async function testEnumerateDiagnosticsMd() {

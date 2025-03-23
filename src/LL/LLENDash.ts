b6e38e1d05da300a853610bbc1a9a0afb727cfa0
hari64 <hari64boli64@gmail.com>
2025-03-23 12:47:06 +0900

    add more exceptions for the LLENDash

--- src/LL/LLENDash.ts
+++ src/LL/LLENDash.ts
@@ -5,66 +5,10 @@ import isLabelOrURL from '../util/isLabelOrURL';
 import match2range from '../util/match2range';
 
 const okWords = [
-    'Award-Winning',
-    'Best-In-Class',
-    'Bottom-Up',
-    'Cutting-Edge',
-    'Data-Driven',
-    'Deep-Learning',
-    'Feature-Based',
-    'Feature-Selection',
-    'First-Order',
     'Fritz-John',
-    'Full-Time',
-    'High-Class',
-    'High-Dimensional',
-    'High-End',
-    'High-Quality',
-    'Higher-Order',
-    'Ill-Defined',
-    'Ill-Posed',
-    'Long-Term',
-    'Low-Dimensional',
-    'Machine-Learning',
-    'Non-Convex',
-    'Non-Empty',
-    'Non-Empty',
-    'Non-Linear',
-    'Non-Negative',
-    'Non-Positive',
-    'Non-Zero',
-    'Open-Source',
-    'Part-Time',
-    'Pre-Processing',
-    'Pop-Culture',
-    'Real-Time',
-    'Reinforcement-Learning',
-    'Second-Order',
-    'Short-Term',
-    'State-Of-The-Art',
-    'Third-Order',
-    'Top-Down',
-    'Top-Rated',
-    'User-Friendly',
-    'Well-Being',
-    'Well-Defined',
-    'Well-Documented',
-    'Well-Known',
-    'Well-Posed',
-    'Zero-Sum',
 ];
 
-// function binarySearch(word: string) {
-//     let low = 0, high = okWords.length;
-//     while (low < high) {
-//         const mid = Math.floor((low + high) / 2);
-//         const cmp = okWords[mid].localeCompare(word);
-//         if (cmp < 0) low = mid + 1;
-//         else if (cmp > 0) high = mid;
-//         else return true;
-//     }
-//     return false;
-// }
+const okPrefixes = ['Anti', 'Auto', 'Award', 'Best', 'Bi', 'Bottom', 'Co', 'Cross', 'Cutting', 'Data', 'De', 'Deep', 'Dis', 'En', 'Ex', 'Feature', 'First', 'Full', 'High', 'Higher', 'Hyper', 'Ill', 'Infra', 'Inter', 'Intra', 'Long', 'Low', 'Machine', 'Macro', 'Micro', 'Multi', 'Neo', 'Non', 'Open', 'Out', 'Over', 'Pan', 'Part', 'Pop', 'Post', 'Post', 'Post', 'Pre', 'Pre', 'Pre', 'Pro', 'Re', 'Real', 'Reinforcement', 'Retro', 'Semi', 'Short', 'State', 'Sub', 'Super', 'Third', 'Top', 'Trans', 'Tri', 'Ultra', 'Ultra', 'Un', 'Under', 'Uni', 'User', 'Well', 'Zero'];
 
 export default function LLENDash(doc: vscode.TextDocument, txt: string): vscode.Diagnostic[] {
     const ranges: vscode.Range[] = [];
@@ -72,6 +16,8 @@ export default function LLENDash(doc: vscode.TextDocument, txt: string): vscode.
     for (const match of txt.matchAll(/[A-Z][a-zA-Z]*[a-z](?:-[A-Z][a-zA-Z]*[a-z])+/g)) {
         // Compared to the binary search, this is faster for small arrays.
         if (okWords.includes(match[0])) continue;
+        const prefix = match[0].split('-')[0];
+        if (okPrefixes.includes(prefix)) continue;
         if (isLabelOrURL(txt, match)) continue;
         ranges.push(match2range(doc, match));
     }

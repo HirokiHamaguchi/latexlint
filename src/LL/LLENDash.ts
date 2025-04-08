e37cfc32bbda313420a5846aec5a850f2c1969c5
hari64 <hari64boli64@gmail.com>
2025-04-08 21:35:47 +0900

    enhance okWords for LLENDash

--- src/LL/LLENDash.ts
+++ src/LL/LLENDash.ts
@@ -8,7 +8,9 @@ const okWords = [
     'Fritz-John',
 ];
 
-const okPrefixes = ['Anti', 'Auto', 'Award', 'Best', 'Bi', 'Bottom', 'Co', 'Cross', 'Cutting', 'Data', 'De', 'Deep', 'Dis', 'En', 'Ex', 'Feature', 'First', 'Full', 'High', 'Higher', 'Hyper', 'Ill', 'Infra', 'Inter', 'Intra', 'Long', 'Low', 'Machine', 'Macro', 'Micro', 'Multi', 'Neo', 'Non', 'Open', 'Out', 'Over', 'Pan', 'Part', 'Pop', 'Post', 'Post', 'Post', 'Pre', 'Pre', 'Pre', 'Pro', 'Prox', 'Proximal', 'Re', 'Real', 'Reinforcement', 'Retro', 'Semi', 'Short', 'State', 'Sub', 'Super', 'Third', 'Top', 'Trans', 'Tri', 'Ultra', 'Ultra', 'Un', 'Under', 'Uni', 'User', 'Well', 'Zero'];
+const okPrefixes = ['Anti', 'Auto', 'Award', 'Best', 'Bi', 'Bottom', 'Co', 'Cross', 'Cutting', 'Data', 'De', 'Deep', 'Dis', 'En', 'Ex', 'Feature', 'First', 'Full', 'High', 'Higher', 'Hyper', 'Ill', 'Infra', 'Inter', 'Intra', 'Long', 'Low', 'Machine', 'Macro', 'Micro', 'Multi', 'Neo', 'Non', 'Open', 'Out', 'Over', 'Pan', 'Part', 'Pop', 'Post', 'Post', 'Post', 'Pre', 'Pre', 'Pre', 'Pro', 'Prox', 'Proximal', 'Rank', 'Re', 'Real', 'Reinforcement', 'Retro', 'Semi', 'Short', 'State', 'Sub', 'Super', 'Third', 'Top', 'Trans', 'Tri', 'Ultra', 'Ultra', 'Un', 'Under', 'Uni', 'User', 'Well', 'Zero'];
+
+const okSuffixes = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
 
 export default function LLENDash(doc: vscode.TextDocument, txt: string): vscode.Diagnostic[] {
     const ranges: vscode.Range[] = [];
@@ -16,8 +18,11 @@ export default function LLENDash(doc: vscode.TextDocument, txt: string): vscode.
     for (const match of txt.matchAll(/[A-Z][a-zA-Z]*[a-z](?:-[A-Z][a-zA-Z]*[a-z])+/g)) {
         // Compared to the binary search, this is faster for small arrays.
         if (okWords.includes(match[0])) continue;
-        const prefix = match[0].split('-')[0];
+        const words = match[0].split('-');
+        const prefix = words[0];
+        const suffix = words[words.length - 1];
         if (okPrefixes.includes(prefix)) continue;
+        if (okSuffixes.includes(suffix)) continue;
         if (isLabelOrURL(txt, match)) continue;
         ranges.push(match2range(doc, match));
     }

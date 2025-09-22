202ccf563ff9de85d3b06232e0b8d14e7db38bbd
Hiroki Hamaguchi <78325943+HirokiHamaguchi@users.noreply.github.com>
2025-09-22 15:31:39 +0900

    update files

--- script/main.py
+++ script/main.py
@@ -1,9 +1,12 @@
+import os
+
 from make_constants_ts import make_constants_ts
 from make_enumerateDiagnostics_ts import make_enumerateDiagnostics_ts
 from make_lint_tex import make_lint_tex
 from make_package_json import make_package_json
 from make_png_files import make_png_files
 from make_readme_md import make_readme_md
+from test_file_content import test_file_content
 from test_is_for_md import test_is_for_md
 from test_mark_down_png import test_mark_down_png
 from test_rule_names import test_rule_names
@@ -18,3 +21,18 @@ if __name__ == "__main__":
     test_is_for_md()
     test_rule_names()
     test_mark_down_png()
+    test_rule_names()
+    test_file_content()
+
+    files = os.listdir(os.path.dirname(__file__))
+    functions = []
+    for f in files:
+        if not f.endswith(".py"):
+            continue
+        if f.startswith("make_") or f.startswith("test_"):
+            functions.append(f[:-3])
+
+    for func in functions:
+        if func not in globals():
+            raise ValueError(f"{func} is not defined")
+    print("All done!")
---
+++ script/test_file_content.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+from get_rule_names import get_rule_names
+
+
+def test_file_content():
+    rule_dir, rule_names = get_rule_names()
+
+    for rule in rule_names:
+        files = os.listdir(rule_dir / rule)
+        assert "lint.tex" in files
+        assert "README.md" in files
+        rule_pdf = f"{rule}.pdf" in files
+        rule_png = f"{rule}.png" in files
+        rule_tex = f"{rule}.tex" in files
+        assert rule_pdf == rule_png == rule_tex, (
+            f"{rule}: {rule_pdf}, {rule_png}, {rule_tex}"
+        )
+
+        assert "values.json" in files
+        with open(rule_dir / rule / "values.json", encoding="utf-8") as f:
+            values = json.load(f)
+        assert isinstance(values, dict), f"{rule}/values.json is not a dict"
+        assert values.keys() == {"message", "severity"}, (
+            f"{rule}/values.json has unexpected keys: {values.keys()}"
+        )
+
+    print("test_file_content ok!")
+
+
+if __name__ == "__main__":
+    test_file_content()

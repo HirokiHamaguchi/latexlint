123d8e355d8d6cde3d7eb43033c6e8981958b8ea
Hiroki Hamaguchi <78325943+HirokiHamaguchi@users.noreply.github.com>
2025-09-22 14:46:53 +0900

    update regex2diagnostics.ts

--- src/util/regex2diagnostics.ts
+++ src/util/regex2diagnostics.ts
@@ -3,18 +3,6 @@ import { LLCode, messages } from './constants';
 import ranges2diagnostics from './ranges2diagnostics';
 import match2range from './match2range';
 
-const correctCommandsColonEqq: { [key: string]: string } = {
-    "::=": "\\Coloneqq",
-    "=::": "\\Eqqcolon",
-    ":=": "\\coloneqq",
-    "=:": "\\eqqcolon"
-};
-
-const correctCommandsLlGg: { [key: string]: string } = {
-    "<<": "\\ll",
-    ">>": "\\gg",
-};
-
 export default function regex2diagnostics(
     doc: vscode.TextDocument,
     txt: string,
@@ -26,12 +14,33 @@ export default function regex2diagnostics(
     for (const match of txt.matchAll(pattern)) {
         const range = match2range(doc, match);
         let mes = messages[code];
-        if (code === "LLBig") mes = mes.replaceAll("%1", match[1]);
-        else if (code === "LLBracketCurly") mes = mes.replaceAll("%1", match[1]);
-        else if (code === "LLColonEqq") mes = mes.replaceAll("%1", correctCommandsColonEqq[match[0]]).replaceAll("%2", match[0]);
-        else if (code === "LLLlGg") mes = mes.replaceAll("%1", correctCommandsLlGg[match[0]]).replaceAll("%2", match[0]);
-        else if (code === "LLThousands") mes = mes.replaceAll("%1", match[1]).replaceAll("%2", match[2]);
-        else if (code === "LLUserDefined") mes = mes.replaceAll("%1", pattern.source);
+
+        /* eslint-disable */
+        if (code === "LLBig") {
+            mes = mes.replaceAll("%1", match[1]);
+        } else if (code === "LLBracketCurly") {
+            mes = mes.replaceAll("%1", match[1]);
+        } else if (code === "LLColonEqq") {
+            const correctCommandsColonEqq: { [key: string]: string } = {
+                "::=": "\\Coloneqq",
+                "=::": "\\Eqqcolon",
+                ":=": "\\coloneqq",
+                "=:": "\\eqqcolon"
+            };
+            mes = mes.replaceAll("%1", correctCommandsColonEqq[match[0]]).replaceAll("%2", match[0]);
+        } else if (code === "LLLlGg") {
+            const correctCommandsLlGg: { [key: string]: string } = {
+                "<<": "\\ll",
+                ">>": "\\gg",
+            };
+            mes = mes.replaceAll("%1", correctCommandsLlGg[match[0]]).replaceAll("%2", match[0]);
+        } else if (code === "LLThousands") {
+            mes = mes.replaceAll("%1", match[1]).replaceAll("%2", match[2]);
+        } else if (code === "LLUserDefined") {
+            mes = mes.replaceAll("%1", pattern.source);
+        }
+        /* eslint-enable */
+
         message.push(mes);
         ranges.push(range);
     }

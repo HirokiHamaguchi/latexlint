8e9c29b4158ec75202cdefc20b9a69f190fbc3ee
Hiroki Hamaguchi <78325943+HirokiHamaguchi@users.noreply.github.com>
2025-09-22 11:23:18 +0900

    fix URI

--- src/util/getCodeWithURI.ts
+++ src/util/getCodeWithURI.ts
@@ -4,6 +4,6 @@ import { LLCode } from "./constants";
 export function getCodeWithURI(code: LLCode) {
     return {
         value: code,
-        target: vscode.Uri.parse(`https://github.com/hari64boli64/latexlint?tab=readme-ov-file#${code.toLowerCase()}`),
+        target: vscode.Uri.parse(`https://github.com/HirokiHamaguchi/latexlint?tab=readme-ov-file#${code.toLowerCase()}`),
     };
 }

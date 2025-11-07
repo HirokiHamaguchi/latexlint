659c50bae90b8ecdcad0e4acda73749c1f1a0f30
Hiroki Hamaguchi <78325943+HirokiHamaguchi@users.noreply.github.com>
2025-11-07 15:37:39 +0900

    fix bug of $

--- web/src/my-text-lint/usage_error.ts
+++ web/src/my-text-lint/usage_error.ts
@@ -55,7 +55,7 @@ function regexCheck(
             errors.push({
                 startOffset: index,
                 endOffset: endIndex,
-                message: `「${match[0]}」は一般に誤用とされており、「${entry.yes.replace('$num', match[1])}」が正しい表現かも知れません。${entry.memo ? entry.memo.join('') : ''}`,
+                message: `「${match[0]}」は一般に誤用とされており、「${entry.yes.replace('$num$', match[1])}」が正しい表現かも知れません。${entry.memo ? entry.memo.join('') : ''}`,
                 code: "usage-error",
             });
             matchedRanges.push([index, endIndex]);
@@ -70,7 +70,7 @@ export function checkUsageError(text: string): MyTextLintErrorResult[] {
     const matchedRanges: [number, number][] = [];
 
     for (const entry of dict) {
-        if (entry.no.includes("$num")) {
+        if (entry.no.includes("$num$")) {
             regexCheck(text, entry, errors, matchedRanges);
         } else {
             naiveCheck(text, entry, errors, matchedRanges);

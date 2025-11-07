266c9cc3950ffdbfb92e401b77024f9307eb0555
Hiroki Hamaguchi <78325943+HirokiHamaguchi@users.noreply.github.com>
2025-11-07 13:58:49 +0900

    fix bug

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { Content } from './content'
 
-// Load kuromoji.js and set it as a global variable
-import * as kuromojiModule from './kuromoji/kuromoji.js';
-(window as Window & { kuromoji?: unknown }).kuromoji = kuromojiModule;
+import './kuromoji/kuromoji.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

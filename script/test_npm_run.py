ded4c569465a637e0c87150123b727e00493e063
Hiroki Hamaguchi <78325943+HirokiHamaguchi@users.noreply.github.com>
2025-12-03 18:27:59 +0900

    fix test_npm_run

--- script/test_npm_run.py
+++ script/test_npm_run.py
@@ -11,8 +11,8 @@ def test_npm_run():
         encoding="utf-8",
     )
     print(f"stdout: {result.stdout}, stderr: {result.stderr}")
-    assert result.returncode == 0, "npm test run failed"
-    print("npm test run succeeded")
+    assert result.returncode == 0, "npm run test failed"
+    print("npm run test succeeded")
 
     result = subprocess.run(
         ["npm", "run", "test"],
@@ -23,8 +23,20 @@ def test_npm_run():
         cwd="web",
     )
     print(f"stdout: {result.stdout}, stderr: {result.stderr}")
-    assert result.returncode == 0, "web/npm test run failed"
-    print("web/npm test run succeeded")
+    assert result.returncode == 0, "web/npm run test failed"
+    print("web/npm run test succeeded")
+
+    result = subprocess.run(
+        ["npm", "run", "build"],
+        capture_output=True,
+        text=True,
+        shell=True,
+        encoding="utf-8",
+        cwd="web",
+    )
+    print(f"stdout: {result.stdout}, stderr: {result.stderr}")
+    assert result.returncode == 0, "web/npm run build failed"
+    print("web/npm run build succeeded")
 
 
 if __name__ == "__main__":

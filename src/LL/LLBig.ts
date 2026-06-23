43cc0aaef4aadf213eadfc5aef14930ba065fb8b
Hiroki Hamaguchi <78325943+HirokiHamaguchi@users.noreply.github.com>
2026-06-23 21:34:32 +0900

    fix bug of LLBig

--- src/LL/LLBig.ts
+++ src/LL/LLBig.ts
@@ -4,12 +4,31 @@ import { messages } from '../util/constants';
 import match2range from '../util/match2range';
 import ranges2diagnostics from '../util/ranges2diagnostics';
 
+function getBracedArgument(text: string, openBraceIndex: number): string | undefined {
+    let depth = 0;
+    if (text[openBraceIndex] !== "{") return text[openBraceIndex];
+    for (let i = openBraceIndex; i < text.length; i++)
+        if (text[i] === "{")
+            depth++;
+        else if (text[i] === "}") {
+            depth--;
+            if (depth === 0) return text.slice(openBraceIndex + 1, i);
+        }
+    return undefined;
+}
+
 export default function LLBig(doc: vscode.TextDocument, txt: LLText): vscode.Diagnostic[] {
     const code = "LLBig";
     let message: string[] = [];
     let ranges: vscode.Range[] = [];
     for (const match of txt.text.matchAll(/\\(cap|cup|odot|oplus|otimes|sqcup|uplus|vee|wedge)_/g)) {
         if (!txt.isValid(match.index)) continue;
+        const subscript = getBracedArgument(txt.text, match.index + match[0].length);
+        if (!subscript) continue;
+        let ok: Boolean = false;
+        if (/(,|:|=|<|>|\\(?:leq?|geq?|in|mid)\b)/.test(subscript)) ok = true;
+        if (subscript.length === 1 && "ijklmn".includes(subscript)) ok = true;
+        if (!ok) continue;
         message.push(messages[code].replaceAll("%1", match[1]));
         ranges.push(match2range(doc, match));
     }
